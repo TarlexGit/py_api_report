533f1c934b1c4edb8035f4dc539d298c3eb17382
Tarlex <Tarlex.py@yandex.ru>
2021-02-02 13:56:17 +0300

    0.2

--- parser.py
+++ parser.py
@@ -27,10 +27,19 @@ class UserHandler:
         str_date_time = today.strftime("%d.%m.%Y %H:%M")
 
         bytes_stream.write("Отчёт для {company}.\n".format(company=str_company["name"]))
-        bytes_stream.write("{name} <{email}> {date}".format(name=str_name, email = str_email, date=str_date_time))
+        bytes_stream.write("{name} <{email}> {date}\n".format(name=str_name, email = str_email, date=str_date_time))
         
-        tasks_values = get_tasks(user.id, tasks_data)
-        print(tasks_values)
+        all_tasks_values = get_tasks(user.id, tasks_data)
+        compile_tasks = [x['title'] for x in all_tasks_values if x['completed'] == True] 
+        not_compile_tasks = [x['title'] for x in all_tasks_values if x['completed'] == False] 
+        
+        bytes_stream.write("Всего задач: {done}\n\nЗавершённые задачи ({comp}):\n".format(done=len(all_tasks_values), comp=len(compile_tasks)))
+        compile_tasks_list=[f'{x}\n' for x in compile_tasks]
+        bytes_stream.write("".join(map(str, compile_tasks_list)))
+
+        not_compile_tasks_list=[f'{x}\n' for x in not_compile_tasks]
+        z="".join(map(str, not_compile_tasks_list))
+        bytes_stream.write("\nОставшиеся задачи ({x}):\n{z}".format(x=len(not_compile_tasks), z=z))
 
         self.create_file(bytes_stream.getvalue())
         
@@ -44,13 +53,17 @@ class UserHandler:
 def get_tasks(user_id, tasks_data):  
     count = [] 
     for item in tasks_data: 
-        for key, value in item.items():
-            if key=='userId' and value==user_id: 
-                count.append(item['completed'] ) 
-    return Counter(count)
+        try:
+            for key, value in item.items():
+                if item['userId']==user_id and item not in count: 
+                    count.append(item)  
+                else:pass 
+        except:pass 
+    return count
 
           
-if __name__ == "__main__":    
+if __name__ == "__main__":   
+    print('api connection') 
     users_url = 'https://json.medrating.org/users'
     users_request = requests.get(url=users_url)
     users_data = users_request.json() # сохраняем от сбоев сети 
@@ -59,11 +72,12 @@ if __name__ == "__main__":
     tasks_request = requests.get(url=tasks_url)
     tasks_data = tasks_request.json()
 
+    print('reporting...')   
     for u in users_data: 
         try:
             user = UserHandler(u)
-            print(user.username)   
             user.make_data()
             
         except AttributeError: pass
+    print('completed') 
         
